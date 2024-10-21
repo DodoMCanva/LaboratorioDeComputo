fc37647cff07b9234072254bf533a5ec91fbf22a
superQ78 <167489927+superQ78@users.noreply.github.com>
2024-10-21 10:24:31 -0700

    pantalla

--- LaboratorioDeComputo/src/main/java/presentacion/frmSeleccionPC.java
+++ LaboratorioDeComputo/src/main/java/presentacion/frmSeleccionPC.java
@@ -31,70 +31,152 @@ public class frmSeleccionPC extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
+        btnPC8 = new javax.swing.JButton();
+        btnPC1 = new javax.swing.JButton();
+        btnPC2 = new javax.swing.JButton();
+        btnPC3 = new javax.swing.JButton();
+        btnPC4 = new javax.swing.JButton();
+        btnPC5 = new javax.swing.JButton();
+        btnPC6 = new javax.swing.JButton();
+        btnPC7 = new javax.swing.JButton();
+        btnSalir = new javax.swing.JButton();
+        lblnumPC1 = new javax.swing.JLabel();
+        lblPC1 = new javax.swing.JLabel();
+        lblnumPC2 = new javax.swing.JLabel();
+        lblPC2 = new javax.swing.JLabel();
+        lblnumPC3 = new javax.swing.JLabel();
+        lblPC3 = new javax.swing.JLabel();
+        lblnumPC4 = new javax.swing.JLabel();
+        lblPC4 = new javax.swing.JLabel();
+        lblnumPC5 = new javax.swing.JLabel();
+        lblPC5 = new javax.swing.JLabel();
+        lblnumPC6 = new javax.swing.JLabel();
+        lblPC6 = new javax.swing.JLabel();
+        lblnumPC7 = new javax.swing.JLabel();
+        lblPC7 = new javax.swing.JLabel();
+        lblnumPC8 = new javax.swing.JLabel();
+        lblPC8 = new javax.swing.JLabel();
+        lblLogo = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(102, 204, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setBackground(new java.awt.Color(0, 153, 255));
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Seleccionar");
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 370, -1, -1));
+        btnPC8.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC8.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC8.setText("Seleccionar");
+        jPanel1.add(btnPC8, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 370, -1, -1));
 
-        jButton2.setBackground(new java.awt.Color(0, 153, 255));
-        jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Seleccionar");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 210, -1, -1));
+        btnPC1.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC1.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC1.setText("Seleccionar");
+        jPanel1.add(btnPC1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 210, -1, -1));
 
-        jButton3.setBackground(new java.awt.Color(0, 153, 255));
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("Seleccionar");
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, -1, -1));
+        btnPC2.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC2.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC2.setText("Seleccionar");
+        jPanel1.add(btnPC2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, -1, -1));
 
-        jButton4.setBackground(new java.awt.Color(0, 153, 255));
-        jButton4.setForeground(new java.awt.Color(255, 255, 255));
-        jButton4.setText("Seleccionar");
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 210, -1, -1));
+        btnPC3.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC3.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC3.setText("Seleccionar");
+        jPanel1.add(btnPC3, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 210, -1, -1));
 
-        jButton5.setBackground(new java.awt.Color(0, 153, 255));
-        jButton5.setForeground(new java.awt.Color(255, 255, 255));
-        jButton5.setText("Seleccionar");
-        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 210, -1, -1));
+        btnPC4.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC4.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC4.setText("Seleccionar");
+        jPanel1.add(btnPC4, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 210, -1, -1));
 
-        jButton6.setBackground(new java.awt.Color(0, 153, 255));
-        jButton6.setForeground(new java.awt.Color(255, 255, 255));
-        jButton6.setText("Seleccionar");
-        jPanel1.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 370, -1, -1));
+        btnPC5.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC5.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC5.setText("Seleccionar");
+        jPanel1.add(btnPC5, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 370, -1, -1));
 
-        jButton7.setBackground(new java.awt.Color(0, 153, 255));
-        jButton7.setForeground(new java.awt.Color(255, 255, 255));
-        jButton7.setText("Seleccionar");
-        jPanel1.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 370, -1, -1));
+        btnPC6.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC6.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC6.setText("Seleccionar");
+        jPanel1.add(btnPC6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 370, -1, -1));
 
-        jButton8.setBackground(new java.awt.Color(0, 153, 255));
-        jButton8.setForeground(new java.awt.Color(255, 255, 255));
-        jButton8.setText("Seleccionar");
-        jPanel1.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 370, -1, -1));
+        btnPC7.setBackground(new java.awt.Color(0, 153, 255));
+        btnPC7.setForeground(new java.awt.Color(255, 255, 255));
+        btnPC7.setText("Seleccionar");
+        jPanel1.add(btnPC7, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 370, -1, -1));
 
-        jButton9.setBackground(new java.awt.Color(255, 51, 51));
-        jButton9.setForeground(new java.awt.Color(255, 255, 255));
-        jButton9.setText("Salir");
-        jButton9.addActionListener(new java.awt.event.ActionListener() {
+        btnSalir.setBackground(new java.awt.Color(255, 51, 51));
+        btnSalir.setForeground(new java.awt.Color(255, 255, 255));
+        btnSalir.setText("Salir");
+        btnSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton9ActionPerformed(evt);
+                btnSalirActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 420, -1, -1));
+        jPanel1.add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 420, -1, -1));
+
+        lblnumPC1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC1.setText("PC 1");
+        jPanel1.add(lblnumPC1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, -1, -1));
+
+        lblPC1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, -1, -1));
+
+        lblnumPC2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC2.setText("PC 2");
+        jPanel1.add(lblnumPC2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 140, -1, -1));
+
+        lblPC2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 110, -1, -1));
+
+        lblnumPC3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC3.setText("PC 3");
+        jPanel1.add(lblnumPC3, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 140, -1, -1));
+
+        lblPC3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC3, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 110, -1, -1));
+
+        lblnumPC4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC4.setText("PC 4");
+        jPanel1.add(lblnumPC4, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 140, -1, -1));
+
+        lblPC4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC4, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 110, -1, -1));
+
+        lblnumPC5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC5.setText("PC 5");
+        jPanel1.add(lblnumPC5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 310, -1, -1));
+
+        lblPC5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 280, -1, -1));
+
+        lblnumPC6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC6.setText("PC 6");
+        jPanel1.add(lblnumPC6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 310, -1, -1));
+
+        lblPC6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 280, -1, -1));
+
+        lblnumPC7.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC7.setText("PC 7");
+        jPanel1.add(lblnumPC7, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 310, -1, -1));
+
+        lblPC7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC7, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 280, -1, -1));
+
+        lblnumPC8.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblnumPC8.setText("PC 8");
+        jPanel1.add(lblnumPC8, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 310, -1, -1));
+
+        lblPC8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/compuITson.png"))); // NOI18N
+        jPanel1.add(lblPC8, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 280, -1, -1));
+
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/logotipoazul.png"))); // NOI18N
+        lblLogo.setText("jLabel8");
+        jPanel1.add(lblLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, 200, 60));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
+        jLabel1.setText("Selecciona tu computadora");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 40, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,9 +192,9 @@ public class frmSeleccionPC extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+    private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton9ActionPerformed
+    }//GEN-LAST:event_btnSalirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -121,15 +203,33 @@ public class frmSeleccionPC extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
+    private javax.swing.JButton btnPC1;
+    private javax.swing.JButton btnPC2;
+    private javax.swing.JButton btnPC3;
+    private javax.swing.JButton btnPC4;
+    private javax.swing.JButton btnPC5;
+    private javax.swing.JButton btnPC6;
+    private javax.swing.JButton btnPC7;
+    private javax.swing.JButton btnPC8;
+    private javax.swing.JButton btnSalir;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblLogo;
+    private javax.swing.JLabel lblPC1;
+    private javax.swing.JLabel lblPC2;
+    private javax.swing.JLabel lblPC3;
+    private javax.swing.JLabel lblPC4;
+    private javax.swing.JLabel lblPC5;
+    private javax.swing.JLabel lblPC6;
+    private javax.swing.JLabel lblPC7;
+    private javax.swing.JLabel lblPC8;
+    private javax.swing.JLabel lblnumPC1;
+    private javax.swing.JLabel lblnumPC2;
+    private javax.swing.JLabel lblnumPC3;
+    private javax.swing.JLabel lblnumPC4;
+    private javax.swing.JLabel lblnumPC5;
+    private javax.swing.JLabel lblnumPC6;
+    private javax.swing.JLabel lblnumPC7;
+    private javax.swing.JLabel lblnumPC8;
     // End of variables declaration//GEN-END:variables
 }
